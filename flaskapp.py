816b7309c79e040da0868908a0aeebbb61d62e0f
xisang233 <632326403@qq.com>
2017-05-18 11:43:12 +0800

    增加了处理URL和ID的功能

--- flaskapp.py
+++ flaskapp.py
@@ -12,6 +12,10 @@ app = Flask('name')
 def index():
     return render_template('hello.html')
 
+@app.route('/hello-j')
+def japanese():
+    return render_template('japanese.html')
+
 @app.route('/search', methods=['GET'])
 def searchBook():
     bookName = request.args.get('book')
@@ -39,9 +43,30 @@ def searchBook():
             bookResult.append(book)
         return render_template('result.html', books = bookResult, bookname = bookName, author = '*')
 
+@app.route('/fromID', methods=['GET'])
+def fromID():
+    number = request.args.get('number')
+    if len(number) < 6:
+        number = '0' * (6-len(number)) + number
+    book = BOOKS[number]
+    return render_template('book.html', book = book, title = book.writer+' '+book.name)
+
+@app.route('/fromURL', methods=['GET'])
+def fromURL():
+    bookCard = request.args.get('url')
+    if 'http://www.aozora.gr.jp/cards/' in bookCard:
+        number = bookCard.split('/')[-1].replace('card', '').replace('.html', '')
+        if len(number) < 6:
+            number = '0' * (6-len(number)) + number
+        book = BOOKS[number]
+        return render_template('book.html', book = book, title = book.writer+' '+book.name)
+    else:
+        return "请检查您输入的url是否正确，必须输入青空文库图书卡的url"
+
 @app.route('/help')
 def help():
     return render_template('help.html')
+
 
 if __name__ == '__main__':
     import pickle
